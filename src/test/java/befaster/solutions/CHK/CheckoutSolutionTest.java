e6aa19ca030d2f4e3aedcb38174689cd0f0d2060
sbx_user1060 <sbx_user1060@ip-10-12-60-19.eu-west-2.compute.internal>
2018-07-01 17:44:38 +0000

    Sun Jul 01 17:44:38 UTC 2018

--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -25,12 +25,17 @@ public class CheckoutSolutionTest {
         assertThat(checkout.checkout("AA"), equalTo(50 + 50));
         assertThat(checkout.checkout("AAA"), equalTo(130));
         assertThat(checkout.checkout("AAAA"), equalTo(130 + 50));
+        assertThat(checkout.checkout("AAAAA"), equalTo(200));
+        assertThat(checkout.checkout("AAAAAA"), equalTo(200 + 50));
         assertThat(checkout.checkout("B"), equalTo(30));
         assertThat(checkout.checkout("BB"), equalTo(45));
         assertThat(checkout.checkout("BBB"), equalTo(45 + 30));
         assertThat(checkout.checkout("C"), equalTo(20));
         assertThat(checkout.checkout("D"), equalTo(15));
-        assertThat(checkout.checkout("ABCD"), equalTo(15 + 20 + 30 + 50));
+        assertThat(checkout.checkout("E"), equalTo(40));
+        assertThat(checkout.checkout("EE"), equalTo(80));
+        assertThat(checkout.checkout("EEB"), equalTo(80));
+        assertThat(checkout.checkout("ABCDE"), equalTo(15 + 20 + 30 + 50 +40));
     }
 
 }
