5df2fed97100c44b317b63fea270f2f287e0200f
sbx_user1060 <sbx_user1060@ip-10-12-60-19.eu-west-2.compute.internal>
2018-07-01 17:07:25 +0000

    Sun Jul 01 17:07:25 UTC 2018

--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -1,5 +1,22 @@
+package befaster.solutions.CHK;
+
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class CheckoutSolutionTest {
+
+    private CheckoutSolution checkout;
+
+    @Before
+    public void setUp() {
+
+        checkout = new CheckoutSolution();
+    }
+
+    @Test
+    public void checkout() throws Exception {
+    }
 
 }
